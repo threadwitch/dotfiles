d83df3fc5f7e7c80c87b079d0b4637730bc72cda
threadweaver <deep.cat0869@fastmail.com>
2026-03-01 22:06:54 -0800

    Tweaked the fish config

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,10 +1,13 @@
 set -Ux COLORTERM truecolor
 set -Ux EDITOR hx
 
-fish_config theme choose "Rosé Pine"
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_config theme choose "Rosé Pine"
+
+    # Set up fzf key bindings
+    fzf --fish | source
+
+    starship init fish | source
 end
 zoxide init fish | source
-starship init fish | source
