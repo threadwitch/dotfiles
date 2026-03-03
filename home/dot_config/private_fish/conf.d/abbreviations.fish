079d48bdbf2bbf1cfdd759852b9ae084fb7a7885
threadweaver <deep.cat0869@fastmail.com>
2026-03-02 22:15:26 -0800

    switched the abbreviations to a symlink

--- home/dot_config/private_fish/conf.d/abbreviations.fish
+++
@@ -1,4 +0,0 @@
-abbr -a -- gca 'git commit -am'
-abbr -a -- zchez 'z /var/home/breakerofhalos/.local/share/chezmoi/home'
-abbr -a -- chzew 'chezmoi edit --watch'
-abbr -a -- chzea 'chezmoi edit --apply'
