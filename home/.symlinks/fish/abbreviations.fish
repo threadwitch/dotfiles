2eaf117b138c3f62ca6ba6f7e49dcd219a18d493
threadweaver <deep.cat0869@fastmail.com>
2026-03-02 22:15:47 -0800

    Helps if I add all the files to git

---
+++ home/.symlinks/fish/abbreviations.fish
@@ -0,0 +1,4 @@
+abbr -a -- gca 'git commit -am'
+abbr -a -- zchez 'z /var/home/breakerofhalos/.local/share/chezmoi/home'
+abbr -a -- chzew 'chezmoi edit --watch'
+abbr -a -- chzea 'chezmoi edit --apply'
