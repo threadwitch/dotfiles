9f9b86c142d4a7d213ca2bf10b5adbb860df02c2
threadweaver <deep.cat0869@fastmail.com>
2026-03-25 23:51:13 -0700

    Added ~/.cargo/bin to the fish_path

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,6 +1,8 @@
 set -Ux COLORTERM truecolor
 set -Ux EDITOR hx
 
+fish_add_path /home/threadweaver/.cargo/bin
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_config theme choose "Rosé Pine"
